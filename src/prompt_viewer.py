7cff4804c325cae52c778a286fee56fbef9a465b
franlucc <lucchetti.f@northeastern.edu>
2024-09-05 14:37:08 -0400

    run code in gradio

--- src/prompt_viewer.py
+++ src/prompt_viewer.py
@@ -3,6 +3,7 @@ from datasets import load_dataset
 import argparse
 import pandas as pd
 from functools import partial
+import subprocess
 """
 Eventually add filters based on headers
 """
@@ -12,7 +13,26 @@ DATATYPES = ["number", "str", "str", "str", "number", "number"]
 SUCCESS_HEADERS = ["is_success", "first_attempt","is_first_success", "last_attempt", "is_last_success"]
 SUCCESS_DATATYPES = ["bool"]*5
 
-def update_components(ds, slider, header_data, success_data, prompt, submitted_text, completion, assertions, prints):
+def capture_output(prompt, completion, prints):
+    code = "\n".join([prompt, "    "+"   \n".join(completion.split("\n")), prints])
+    outputs = subprocess.run(["python", "-c", code], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stderr = gr.Textbox(outputs.stderr.decode("utf-8").strip(), label="Code Errors", type="text")
+    stdout = gr.Code(outputs.stdout.decode("utf-8").strip(), label="Code Ouputs", language="python")
+    return stderr, stdout
+
+def update_components(
+    ds, 
+    slider, 
+    header_data, 
+    success_data, 
+    prompt, 
+    submitted_text, 
+    completion, 
+    assertions, 
+    prints, 
+    code_err, 
+    code_output
+):
     row = ds.iloc[[slider]]
     header_data = gr.Dataframe(
         headers=HEADERS,
@@ -38,7 +58,10 @@ def update_components(ds, slider, header_data, success_data, prompt, submitted_t
     completion = gr.Code(row["completion"], language="python", label="Completion")
     assertions = gr.Code(row["assertions"], language="python", label="Assertions")
     prints = gr.Code(row["prints"], language="python", label="Prints")
-    return slider, header_data, success_data, prompt, submitted_text, completion, assertions, prints
+    code_err = gr.Textbox("__stderr__", label="Code Errors", type="text")
+    code_output = gr.Code("__stdout__", language="python", label="Code Outputs")
+    return [slider, header_data, success_data, prompt, submitted_text, 
+            completion, assertions, prints, code_err, code_output]
         
 def main(args):
     ds = pd.read_parquet("hf://datasets/wellesley-easel/StudentEval/data/test-00000-of-00001.parquet")
@@ -67,32 +90,39 @@ def main(args):
         )
         
         prompt = gr.Code("__prompt__", language="python", label="Prompt")            
-
         submitted_text = gr.Textbox("__submitted_text__", type="text", label="Submitted Text")
-                
         completion = gr.Code("__completion__", language="python", label="Completion")
                 
         with gr.Row():
             assertions = gr.Code("__assertions__", language="python", label="Assertions")
             prints = gr.Code("__prints__", language="python", label="Prints")
         
-        components = [slider, header_data, success_data, prompt, submitted_text, completion, assertions, prints]
- 
-        slider.input(fn=partial(update_components, ds), inputs=components, outputs=components)
+        runbtn = gr.Button("Run this code")
+        
+        with gr.Column():
+            with gr.Row():
+                code_err = gr.Textbox("__stderr__", label="Code Errors", type="text")
+                code_output = gr.Code("__stdout__", language="python", label="Code Outputs")
         
         gr.Markdown("**Logging and Filtering**\n")
         with gr.Column():
             with gr.Row():
-                btn = gr.Button("Flag this example to log file")
-                out = gr.Textbox(label="Num examples in logfile")
+                flagbtn = gr.Button("Flag this example to log file")
+                flagout = gr.Textbox(label="Num examples in logfile")
             
+        # updates                
+        # run code
+        runbtn.click(fn=capture_output, inputs=[prompt, completion, prints], outputs=[code_err,code_output])
+        # change example on slider change
+        components = [slider, header_data, success_data, prompt, submitted_text, completion, assertions, prints, code_err, code_output]
+        slider.input(fn=partial(update_components, ds), inputs=components, outputs=components)
+        # log
         callback.setup(components, "flagged_data_points")
-        btn.click(lambda *args: callback.flag(list(args)), components, out,
+        flagbtn.click(lambda *args: callback.flag(list(args)), components, flagout,
                   preprocess=False, show_progress="full", trigger_mode="once")
-        
+
         # TODO: add filtering options
-        
-        
+
     demo.launch(share=args.share)
     
 if __name__ == "__main__":

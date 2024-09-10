4653cc0b9a61b4acfe5b57bf69883f8df1f279a6
flucchetti <fr.lucchetti@gmail.com>
2024-09-10 16:37:47 +0000

    update run eval

--- src/run_eval.py
+++ src/run_eval.py
@@ -31,30 +31,71 @@ import datasets
 import argparse
 import subprocess
 from multiprocessing import cpu_count
+from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
+from tqdm import tqdm
+import tempfile
+EXECUTION_TIMEOUT = 15
 
+def generate_splits(ds):
+    # save main as csv
+    ds.to_csv("/tmp/studenteval_completions.csv")
+    filtered_data = {}
+    for item in ds:
+        idx = item["__index_level_0__"]
+        if idx in filtered_data.keys():
+            if not filtered_data[idx]["is_success"] and item["is_success"]:
+                filtered_data[idx] = item
+        else:
+            filtered_data[idx] = item
+    # save filtered csv
+    pd.DataFrame.from_records(filtered_data).to_csv("/tmp/studenteval_filtered.csv")
+    return datasets.load_dataset("csv", data_files={
+        "test": "/tmp/studenteval_filtered.csv",
+        "all_completions": "/tmp/studenteval_completions.csv"
+    })
+    
 def run_problem(ex):
     tests_passed = 0
     tests = ["assert "+ a for a in ex["assertions"].split("assert ") if a != ""]
-    assert len(tests) == ex["total_tests"], f'{ex["assertions"]}, {ex["total_tests"]}'
+    assert len(tests) == ex["total_tests"]
     
-    for test in tests:
-        prompt = "'''" + "\n".join([ex["prompt"], "\t".join(ex["completion"].split("\n")), test]) + "'''"
-        output = subprocess.run(
-            ["python", "-c", prompt],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        if not "error" in output.stderr.decode("utf-8").lower():
+    for idx,test in enumerate(tests):
+        prompt = ex["prompt"].strip() + "\n    " + ex["completion"] + test
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".py", prefix=f'{ex["_id"]}_{idx}_') as f:
+        # with open(f"/tmp/{prefix}.py", "w") as f:
+            f.write(prompt)
+            f.flush()
+            try:
+                result = subprocess.run(
+                    ["python3", f.name],
+                    timeout=EXECUTION_TIMEOUT,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                    stdin=subprocess.DEVNULL,
+                )
+                exit_code = result.returncode
+            except subprocess.TimeoutExpired:
+                exit_code = 1
+        
+        if exit_code == 0:
             tests_passed += 1
     assert tests_passed <= len(tests)
-    return {"is_success": tests_passed == len(tests), "tests_passed": tests_passed}
+    return {**ex, "is_success": tests_passed == len(tests), "tests_passed": tests_passed}
 
 def main(args):
-    ds = datasets.load_from_disk(args.dataset)
-    ds = ds.map(lambda x: {"completion_id": x["completion_id"], "temperature": x["temperature"],
-                           **x["extras"]}, desc="Flattening")
+    ds = datasets.load_from_disk(args.dataset).select(range(1000))
+    ds = ds.map(lambda x,i: {**x["extras"], "completion": x["completion"],
+                           "completion_id": x["completion_id"], "temperature": x["temperature"], "_id": i}, desc="Flattening", with_indices=True)
     print(ds)
-    ds = ds.map(lambda x: {**x, **run_problem(x)}, desc="Evaluating", num_proc=cpu_count())
+    with ThreadPoolExecutor(max_workers=cpu_count() - 1) as executor:
+        res = list(tqdm(executor.map(run_problem, ds), total=len(ds)))
+        ds = datasets.Dataset.from_list(res)
+        
+    ds = ds.remove_columns(["extras","_id"])
+    print(ds)
+    # create splits
+    ds = generate_splits(ds)
     print(ds)
     ds.save_to_disk(args.outdataset)
     # num success

9c7b66b3c9b2c4950bfd83107abc2fa6bc4ea079
Arjun Guha <a.guha@northeastern.edu>
2024-09-11 12:55:51 -0400

    After discussion with Carolyn

--- mutated_dataset_builder/main.py
+++ mutated_dataset_builder/main.py
@@ -1,51 +1,61 @@
 import datasets
 from pathlib import Path
+import spacy
 import json
 import argparse
 
+# For each KEY:VALUE pair, when we see the word VALUE in the prompt, we tag it
+# with KEY using tag_prompt.
+TAGS = {
+    "print": "return",
+    "prints": "returns",
 
-def rule_array_to_list(prompt: str) -> str:
-    """
-    Replace all instances of "array" with a list.
-
-    (This is just a demo.)
-    """
-    return prompt.replace("array", "list")
-
-def rule_replace_print_with_return(prompt: str) -> str:
-    """
-    Replace all instances of "print" with "return".
-    """
-    return prompt.replace("print", "return")
-
-RULES = {
-    "array_to_list": rule_array_to_list,
-    "replace_print_with_return": rule_replace_print_with_return,
 }
 
+def tag_prompt(nlp: spacy.Language, prompt: str) -> str:
+    """
+    Given a prompt such as
+
+    This function prints the word "Hello".
+
+    This function will return the following string:
+
+    This function $$returns:prints$$ the $$string:word$$ "Hello".
+
+    Thus, all possible words that we may substitute are tagged with their category.
+
+    This is *not* a dataset that we can feed to the model. But, we can edit the
+    dataset manually to address cases where the rules go wrong. After inspection,
+    we can apply substitutions.
+    """
+    doc = nlp(prompt)
+    new_prompt = []
+    for token in doc:
+        lemma = token.lemma_
+        if token.text in TAGS:
+            new_prompt.append(f"${TAGS[token.text]}:{token.text}")
+        else:
+            new_prompt.append(token.text)
+    return "".join(new_prompt)
+
 def main_with_args(original_dataset: str, output_path: Path):
+    nlp = spacy.load("en_core_web_trf")
     original_dataset = datasets.load_dataset(original_dataset, split="only_subsets")
-    new_dataset = [ ]
-    # also add the original dataset to the new dataset
+    results = [ ]
     for item in original_dataset:
-        new_dataset.append({ **item, "rule": "original", "original_prompt": item["prompt"] })
-
-    for rule_name, rule_function in RULES.items():
-        for item in original_dataset:
-
-            new_prompt = rule_function(item["prompt"])
-            if new_prompt != item["prompt"]:
-                new_dataset.append({ **item, "rule": rule_name, "prompt": new_prompt, "original_prompt": item["prompt"] })
+        original_prompt = item["prompt"]
+        tagged_prompt = tag_prompt(nlp, original_prompt)
+        results.append({ **item, "prompt": tagged_prompt })
     with output_path.open("w") as f:
-        for item in new_dataset:
+        for item in results:
             f.write(json.dumps(item) + "\n")
-    print(f"Saved {len(new_dataset)} items to {output_path}")
+    print(f"Saved {len(results)} items to {output_path}")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--original_dataset", type=str, default="wellesley-easel/StudentEval")
-    parser.add_argument("--output_path", type=Path, default=Path("mutated_dataset.jsonl"))
+    parser.add_argument("--output_path", type=Path, default=Path("tagged_prompts.jsonl"))
     args = parser.parse_args()
     main_with_args(args.original_dataset, args.output_path)
 

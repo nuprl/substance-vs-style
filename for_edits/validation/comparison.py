b217085a7a7425731f5f357fde7e00ec02f6ac3b
Molly Feldman <mfeldman@oberlin.edu>
2024-10-02 13:39:28 -0400

    ready for decision making

--- for_edits/validation/comparison.py
+++ for_edits/validation/comparison.py
@@ -1,19 +1,15 @@
 from deepdiff import DeepDiff
 import yaml 
 
-first_half = ['Carolyn_annotated_two_of_each_firstlast_firsthalf.yaml', 'two_of_each_firstlast_firsthalf_molly.yaml']
-second_half = ['Carolyn_annotated_two_of_each_firstlast_secondhalf.yaml', 'two_of_each_firstlast_secondhalf_molly.yaml']
+#first_half = ['Carolyn_annotated_two_of_each_firstlast_firsthalf.yaml', 'two_of_each_firstlast_firsthalf_molly.yaml']
+#second_half = ['Carolyn_annotated_two_of_each_firstlast_secondhalf.yaml', 'two_of_each_firstlast_secondhalf_molly.yaml']
 
-for v in [first_half, second_half]:
-    with open(v[0], 'r') as c:
-        carolyn = yaml.safe_load(c)
-    with open(v[1], 'r') as q:
-        molly = yaml.safe_load(q)
-    diff = DeepDiff(carolyn, molly, verbose_level=1)
-    print(diff)
-
-    #print(diff.pretty())
-    #print(DeepDiff(carolyn, molly).pretty())
-    print()
-    print("***********************")
-    print()
+v = ['carolyn_subsetted_firstlast.yaml', 'subsetted_firstlast_molly.yaml']
+with open(v[0], 'r') as c:
+    carolyn = yaml.safe_load(c)
+with open(v[1], 'r') as q:
+    molly = yaml.safe_load(q)
+diff = DeepDiff(carolyn, molly, verbose_level=1)
+#print(diff)
+print(diff.pretty())
+#print(DeepDiff(carolyn, molly).pretty())

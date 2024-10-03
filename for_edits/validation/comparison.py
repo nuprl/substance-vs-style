c285e1c2d9eb7ffb84cb2da5040fc1382646e436
Molly Feldman <mfeldman@oberlin.edu>
2024-10-03 15:08:07 -0400

    better organization for_edits

--- for_edits/validation/comparison.py
+++ for_edits/validation/comparison.py
@@ -4,7 +4,8 @@ import yaml
 #first_half = ['Carolyn_annotated_two_of_each_firstlast_firsthalf.yaml', 'two_of_each_firstlast_firsthalf_molly.yaml']
 #second_half = ['Carolyn_annotated_two_of_each_firstlast_secondhalf.yaml', 'two_of_each_firstlast_secondhalf_molly.yaml']
 
-v = ['carolyn_subsetted_firstlast.yaml', 'subsetted_firstlast_molly.yaml']
+#v = ['carolyn_subsetted_firstlast.yaml', 'subsetted_firstlast_molly.yaml']
+v = ['Carolyn_subsetted_firstlast_interim_concensus.yaml', 'subsetted_firstlast_interim_concensus_molly.yaml']
 with open(v[0], 'r') as c:
     carolyn = yaml.safe_load(c)
 with open(v[1], 'r') as q:
